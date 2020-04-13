3a4ef0b0b344fea32d1a7462629045c795f6a013
unknown <rajesh.raash@gmail.com>
2020-04-13 17:21:40 +0530

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -145,12 +145,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#EMAIL config
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='rajesh.raash@gmail.com'
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
+
 
 try:
     from .local_settings import *
